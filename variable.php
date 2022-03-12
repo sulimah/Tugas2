0998a9224cf8594a06887a7de36beaa653faf4a9
sulimah <sulimeeee@gmail.com>
2022-03-12 22:49:51 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+    $A = 10;
+    $B = 20;
+    $C = 50;
+    $hasil_P = $A + $B;
+    $hasil_K = $C - $A;
+    $hasil_L = $B * $A;
+    
+
+    echo ("Penjumlahan");
+    echo "<br>";
+    echo "$A + $B = $hasil_P";
+    echo "<br>";
+    echo ("Pengurangan");
+    echo "<br>";
+    echo "$C - $A = $hasil_K";
+    echo "<br>";
+    echo ("Perkalian");
+    echo "<br>";
+    echo "$B + $A = $hasil_L";
+?>
