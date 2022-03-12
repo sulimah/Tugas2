d7cf3b136c538e6750dc8ef0c9f6f3b0ced78214
sulimah <sulimeeee@gmail.com>
2022-03-12 22:51:33 +0700

    menambahkan fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,17 @@
+<?php
+    function Nama (){
+        return "SULIMAH";
+    }
+    function NIM (){
+        return "200411100054";
+    }
+    function PRODI (){
+        return "Teknik Informatika";
+    }
+
+    echo ("SULIMAH");
+    echo "<br>";
+    echo ("200411100054");
+    echo "<br>";
+    echo ("Teknik Informatika");
+?>
